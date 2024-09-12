fe30b4052d867fd6eadd44b161c2b45adf25afd1
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2024-09-12 14:48:24 +0900

    Implement slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { createGlobalStyle } from 'styled-components';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import Path from './Path';
 
 const GlobalStyle = createGlobalStyle`
 ${reset}
@@ -18,14 +19,86 @@ a {
   text-decoration:none;
   color:inherit;
 }
-
 `;
 
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  position: absolute;
+  top: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 27px;
+`;
+
+const boxVariants = {
+  entry: (isBack: boolean) => ({
+    x: isBack ? -500 : 500,
+    opacity: 0,
+    scale: 0,
+  }),
+  center: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: (isBack: boolean) => ({
+    x: isBack ? 500 : -500,
+    opacity: 0,
+    scale: 0,
+    transition: {
+      duration: 0.5,
+    },
+  }),
+};
+
 export default function App() {
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const goNext = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const goPrev = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+
   return (
     <>
       <GlobalStyle />
-      <Path />
+      <Wrapper>
+        <AnimatePresence custom={back}>
+          <Box
+            custom={back}
+            variants={boxVariants}
+            initial='entry'
+            animate='center'
+            exit='exit'
+            key={visible}
+          >
+            {visible}
+          </Box>
+        </AnimatePresence>
+        <button onClick={goPrev}>prev</button>
+        <button onClick={goNext}>next</button>
+      </Wrapper>
     </>
   );
 }
